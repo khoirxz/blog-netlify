bfe90e2c37d20dfe541b5b7a97a06584eb4670bf
N3tw0rk <khoirrurrohman@gmail.com>
2020-11-05 19:03:00 +0700

    header

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const BlogIndex = ({ data, location }) => {
 
   if (posts.length === 0) {
     return (
-      <Layout location={location} title={siteTitle}>
+      <Layout location={location} title="Madingnya Rizqi 😎">
         <SEO title="All posts" />
         <Bio />
         <p>
